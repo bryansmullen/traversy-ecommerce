959c133f6184bbf02a394dcad69287d9230bc705
Bryan Mullen <61456707+bryansmullen@users.noreply.github.com>
2022-09-25 18:21:33 +0100

    Build Front End

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
     return (
-        <>
-            <h1>
-                Welcome to Proshop
-            </h1></>
-    );
+        <BrowserRouter>
+            <Header />
+            <main className={'py-3'}>
+                <Container>
+                    <Switch>
+                        <Route path={'/'} component={HomeScreen} exact={true} />
+                        <Route
+                            path={'/product/:id'}
+                            component={ProductScreen}
+                        />
+                    </Switch>
+                </Container>
+            </main>
+            <Footer />
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+function Footer() {
+    return (
+        <Container>
+            <Row>
+                <Col className={'text-center py-3'}>
+                    <footer>Copyright &copy; ProShop</footer>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Footer
